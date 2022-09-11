6634ddbb4720b2cc0a6e15a46bcc1406e1511412
Pratyush Manav <pratyush.manav@in.abb.com>
2022-09-11 23:01:21 +0530

    for_loop

--- Basics/inference.go
+++ Basics/inference.go
@@ -54,3 +54,24 @@ func conditionals() {
 		fmt.Println("The number is negative")
 	}
 }
+
+func for_loop() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println("sum = ", sum)
+	for_loop2()
+}
+
+func for_loop2() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	sum := 0
+
+	for _, num := range nums {
+		sum += num
+	}
+
+	fmt.Println(sum)
+}
--- Basics/simple.go
+++ Basics/simple.go
@@ -15,4 +15,5 @@ func main() {
 	shorthand()
 	UserInput()
 	conditionals()
+	for_loop()
 }
